3b08dfeb571eb83ecb352ab59296acf19f9d7d35
Diego <diegolm73@yahoo.com.ar>
2021-04-13 21:36:53 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './temaConfig'
+import Navbar from './components/NavBar';
+import App from 'App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return ( <
+        ThemeProvider theme = { theme } >
+        <
+        Navbar / >
+        <
 
+        Button variant = "contained"
+        color = "primary" >
+        vINOS TINTOS <
+        /Button> <
+        Button variant = "contained"
+        color = "primary" >
+        VINOS BLANCOS <
+        /Button> <
+        Button variant = "contained"
+        color = "primary" >
+        VINOS ESPUMANTES <
+        /Button> < /
+        ThemeProvider >
+
+    );
+}
 export default App;
